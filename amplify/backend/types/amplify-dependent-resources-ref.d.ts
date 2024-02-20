9be3f49950aa1c8ec34fb77b6589adb2afc00b54
yeonsu <bestdustn@sookmyung.ac.kr>
2024-02-20 16:14:52 +0900

    amplify setting

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,35 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "laundrycoachfe": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "laundrycoachfee69e9284": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Trigger2cd553a6": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "imagestorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
