76f8548415648e35681c241f5dd829e66306b63e
yeonsu <bestdustn@sookmyung.ac.kr>
2024-02-21 16:44:32 +0900

    [FIX] Update GPT summary - openai version update로 인해  openai.chat.completions.create 부분 코드가 달라짐. - 검색 결과가 없는 경우 조건문 수정

--- amplify/backend/function/clothSearchFunction/src/gpt_summary.js
+++ amplify/backend/function/clothSearchFunction/src/gpt_summary.js
@@ -4,7 +4,7 @@ const OpenAI = require('openai');
 const chatGpt = async (prompt, model = "gpt-3.5-turbo") => {
     const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-    const completion = await openai.ChatCompletion.create({
+    const chatCompletion = await openai.chat.completions.create({
         model: model,
         messages: [
             { "role": "system", "content": "You are expert in laundry. Please explain materials and washing method based on the data sent by the user in Korean." }, 
@@ -12,18 +12,21 @@ const chatGpt = async (prompt, model = "gpt-3.5-turbo") => {
         ]
     });
 
-    return completion.choices[0].message.content;
+    return chatCompletion.choices[0].message.content;
 };
 
 // GPT-3.5 Turbo를 사용하여 요약 정리
 const summarizeWithGpt = async (organizedSearchResult) => {
-    if (!organizedSearchResult.length) {
+    console.log('organizedSearchResult:', organizedSearchResult);
+
+    if (!Object.keys(organizedSearchResult).length) {
         return "검색 결과가 없습니다.";
     }
 
-    const prompt = `${organizedSearchResult.materils}: 소재에 대해 설명해줘. 그리고, ${organizedSearchResult.methods}: 세탁방법도 한국어로 친절하게 설명해줘.`;
+    const prompt = `${organizedSearchResult.materials}: 소재에 대해 설명해줘. 그리고, ${organizedSearchResult.howToLaundry_desc}: 세탁방법도 한국어로 친절하게 설명해줘.`;
     const answer = await chatGpt(prompt);
 
     return answer.trim();
 };
 
+module.exports = { summarizeWithGpt };
